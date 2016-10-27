1e3899376e4c2d523c0cb7eb3f572cd5fa21fd72
Volodymyr Sapsai <vsapsai@gmail.com>
2016-10-26 19:50:55 -0700

    Show stacking contexts.

--- src/content-script.js
+++ src/content-script.js
@@ -12,16 +12,29 @@
         }
     });
 
-    if (visibleForTesting && (visibleForTesting.id === "z-index debug plugin")) {
+    if ((typeof visibleForTesting !== "undefined") && visibleForTesting && (visibleForTesting.id === "z-index debug plugin")) {
         visibleForTesting.exports.getElementTreeData = getElementTreeData;
     }
 
     function getElementTreeData(element) {
         var result = {
-            rect: [element.offsetLeft, element.offsetTop, element.offsetWidth, element.offsetHeight]
+            rect: [element.offsetLeft, element.offsetTop, element.offsetWidth, element.offsetHeight],
+            tagName: element.tagName
         };
-        var resultChildren = [];
+        // In HTML <p> offsetTop if position of the element's bottom and on canvas `y` is position of rect top.
+        if (element.tagName === "P") {
+            result.rect[1] -= result.rect[3];
+        }
+        // z-index
+        var computedStyle = window.getComputedStyle(element);
+        result.computedZIndex = computedStyle.zIndex;
+
+        // Stacking context.
+        result.isStackingContextRoot = ((computedStyle.zIndex != "auto") &&
+            ((computedStyle.position === "absolute") || (computedStyle.position === "relative")));
+
         // Recursively add children.
+        var resultChildren = [];
         var children = element.children;
         if (children) {
             var i;
--- src/panel.js
+++ src/panel.js
@@ -2,6 +2,12 @@
     "use strict";
 
     var canvas = document.getElementById("canvas");
+    // Use colors from D3 schemeCategory20
+    // https://github.com/d3/d3-scale/blob/master/README.md#schemeCategory20
+    var colors = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c",
+                  "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5",
+                  "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f",
+                  "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"];
 
     // Create a connection to the background page.
     var backgroundPageConnection = chrome.runtime.connect({
@@ -30,13 +36,90 @@
         canvas.width = data.rect[2];
         canvas.height = data.rect[3];
 
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "green";
+        var stackingContextTree = buildStackingContextTree(data);
+        drawStackingContextTree(stackingContextTree);
+    }
+
+    function  buildStackingContextTree(elementTree) {
+        var offsetLeft = elementTree.rect[0],
+            offsetTop = elementTree.rect[1],
+            width = elementTree.rect[2],
+            height = elementTree.rect[3];
+        var root = {
+            zIndex: elementTree.computedZIndex,
+            elementRectangles: [[offsetLeft, offsetTop, width, height]],
+            children: []
+        };
         var i;
-        for (i = 0; i < data.children.length; i++) {
-            var childRect = data.children[i].rect;
-            // In HTML offsetTop if position of the element's bottom and on canvas `y` is position of rect top.
-            ctx.fillRect(childRect[0], childRect[1] - childRect[3], childRect[2], childRect[3]);
+        for (i = 0; i < elementTree.children.length; i++) {
+            buildStackingContextTreeRecursive(root, elementTree.children[i], offsetLeft, offsetTop);
         }
+        return root;
+    }
+
+    function buildStackingContextTreeRecursive(node, elementTree, parentOffsetLeft, parentOffsetTop) {
+        var offsetLeft = elementTree.rect[0] + parentOffsetLeft;
+        // In HTML offsetTop if position of the element's bottom and on canvas `y` is position of rect top.
+        // It's not applicable to root element because it doesn't have `offsetParent`.
+        var offsetTop = elementTree.rect[1] + parentOffsetTop;
+        var elementRect = [offsetLeft, offsetTop, elementTree.rect[2], elementTree.rect[3]];
+        var currentNode;
+        if (elementTree.isStackingContextRoot) {
+            currentNode = {
+                zIndex: elementTree.computedZIndex,
+                elementRectangles: [elementRect],
+                children: []
+            };
+            node.children.push(currentNode);
+        } else {
+            node.elementRectangles.push(elementRect);
+            currentNode = node;
+        }
+        var i;
+        for (i = 0; i < elementTree.children.length; i++) {
+            buildStackingContextTreeRecursive(currentNode, elementTree.children[i], offsetLeft, offsetTop);
+        }
+    }
+
+    function drawStackingContextTree(stackingContextTree) {
+        var context = canvas.getContext("2d");
+        drawStackingContextTreeRecursive(context, stackingContextTree, false, 0);
+    }
+
+    // Returns number of used colors which is the same as number of stacking contexts in the tree excluding root.
+    function drawStackingContextTreeRecursive(context, stackingContextTree, shouldFillRectangles, colorIndex) {
+        var count = 0;
+        var i;
+        if (shouldFillRectangles) {
+            context.fillStyle = getColor(colorIndex);
+            for (i = 0; i < stackingContextTree.elementRectangles.length; i++) {
+                var rect = stackingContextTree.elementRectangles[i];
+                context.fillRect(rect[0], rect[1], rect[2], rect[3]);
+            }
+            count++;
+        }
+        if (stackingContextTree.children.length > 0) {
+            var children = Array.from(stackingContextTree.children);
+            children.sort(function(node1, node2) {
+                if (node1.zIndex === node2.zIndex) {
+                    return 0;
+                }
+                if (node1.zIndex === "auto") {
+                    return -1;
+                }
+                if (node2.zIndex === "auto") {
+                    return 1;
+                }
+                return parseInt(node1.zIndex, 10) - parseInt(node2.zIndex, 10);
+            });
+            for (i = 0; i < children.length; i++) {
+                count += drawStackingContextTreeRecursive(context, children[i], true, colorIndex + count);
+            }
+        }
+        return count;
+    }
+
+    function getColor(index) {
+        return colors[index % colors.length];
     }
 })();
